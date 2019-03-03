1c0f20989362ee50eba77da8d066881b1a79ad6d
root <root@server2.bentsea.net>
2019-03-03 07:55:52 +0000

    Blerg

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,15 +3,10 @@
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     lftp -u ${USER},${PASS} ${HOST} <<EOF
-    set ftp:ssl-allow false
-    rm -r ./public_html
-    mkdir public_html
-    bye
-EOF
-    cd _site || exit
-    lftp -u ${USER},${PASS} ${HOST} <<EOF
-    set ftp:ssl-allow false
-    mirror -R --parallel -c . ./public_html
-    bye
+set ftp:ssl-allow false
+rm -r public_html
+mkdir public_html
+mirror -R --parallel -c _site/ ./public_html
+bye
 EOF
 fi
