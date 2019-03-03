af82402efb031d29d4b35a73c9b9a524665b4437
root <root@server2.bentsea.net>
2019-03-03 08:54:53 +0000

    Deployment testing.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,12 +2,10 @@
 
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
-    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf public_html
+    lftp -u $USERNAME,$PASSWORD sftp://$HOST<<EOF
+    rm -r public_html
     mkdir public_html
-    quit
+    mirror -R --parallel -c _site/ ./public_html
+    bye
 EOF
-
-    cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /public_html .
 fi
